585688808f0177861aa00634e366dd4c0cdd4309
David <david.bujok@gmx.de>
2014-07-11 17:48:38 +0200

    graph implemented

--- src/web/components/formmanagement/app.js
+++ src/web/components/formmanagement/app.js
@@ -30,9 +30,57 @@ formmanagement.run(["Session", "showLoginDialog", function (Session, showLoginDi
 
 }]);
 
-formmanagement.controller("NavbarCtrl", ["$scope", "Session", function ($scope, Session) {
-   $scope.logout = function(){
+formmanagement.controller("NavbarCtrl", ["$scope", "Session", "getUserClass", function ($scope, Session, getUserClass) {
+
+    $scope.session = Session.get();
+    //console.log($scope.session.user);
+   // $scope.userClass = getUserClass($scope.session.user.type);
+
+    $scope.logout = function(){
        Session.logout();
    } ;
 }]);
+
+formmanagement.directive('chart', function () {
+    var baseWidth = 600;
+    var baseHeight = 400;
+
+    return {
+      restrict: 'E',
+      template: '<canvas></canvas>',
+      scope: {
+        chartObject: "=value"
+      },
+      link: function (scope, element, attrs) {
+        var canvas  = element.find('canvas')[0],
+            context = canvas.getContext('2d'),
+            chart;
+
+        var options = {
+          type:   attrs.type   || "Line",
+          width:  attrs.width  || baseWidth,
+          height: attrs.height || baseHeight
+        };
+        canvas.width = options.width;
+        canvas.height = options.height;
+        chart = new Chart(context);
+
+        scope.$watch(function(){ return element.attr('type'); }, function(value){
+          if(!value) return;
+          options.type = value;
+          var chartType = options.type;
+          chart[chartType](scope.chartObject.data, scope.chartObject.options);
+        });
+
+        //Update when charts data changes
+        scope.$watch(function() { return scope.chartObject; }, function(value) {
+          if(!value) return;
+          var chartType = options.type;
+          chart[chartType](scope.chartObject.data, scope.chartObject.options);
+        });
+      }
+
+    }
+  });
+
 
--- src/web/components/formmanagement/patients/patients.js
+++ src/web/components/formmanagement/patients/patients.js
@@ -17,7 +17,107 @@ patients.controller("patientsCtrl", ["$scope", "Session", "Patient", "Physician"
     $scope.physicains = Physician.query();
     console.log($scope.session);
 
-}]);
+
+    $scope.updateData = function () {
+        var chart = document.getElementById("myCoolChart").getAttribute("type");
+        switch (chart) {
+            case 'PolarArea':
+                $scope.generatePieData();
+                break;
+            case 'Pie':
+                $scope.generatePieData();
+                break;
+            case 'Doughnut':
+                $scope.generatePieData();
+                break;
+            default:
+                $scope.generateData();
+        }
+    };
+
+    $scope.generateData = function () {
+        var sevenRandNumbers = function () {
+            var numberArray = [];
+            for (var i = 0; i < 7; i++) {
+                numberArray.push(Math.floor((Math.random() * 100) + 1));
+            }
+            return numberArray;
+        };
+        var data = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    fillColor: "rgba(220,220,220,0.5)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    data: sevenRandNumbers()
+                },
+                {
+                    fillColor: "rgba(151,187,205,0.5)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    data: sevenRandNumbers()
+                }
+            ]
+        };
+        $scope.myChart = {"data": data, "options": {} };
+    };
+
+    $scope.generatePieData = function () {
+        var data = [
+            {
+                value: Math.floor((Math.random() * 100) + 1),
+                color: "#F38630"
+            },
+            {
+                value: Math.floor((Math.random() * 100) + 1),
+                color: "#E0E4CC"
+            },
+            {
+                value: Math.floor((Math.random() * 100) + 1),
+                color: "#69D2E7"
+            }
+        ]
+        $scope.myChart = {"data": data, "options": {} };
+    };
+
+    $scope.line = function () {
+        document.getElementById('myCoolChart').setAttribute('type', 'Line');
+        $scope.updateData();
+    };
+
+    $scope.bar = function () {
+        document.getElementById('myCoolChart').setAttribute('type', 'Bar');
+        $scope.updateData();
+    };
+
+    $scope.radar = function () {
+        document.getElementById('myCoolChart').setAttribute('type', 'Radar');
+        $scope.updateData();
+    };
+
+    $scope.polarArea = function () {
+        document.getElementById('myCoolChart').setAttribute('type', 'PolarArea');
+        $scope.updateData();
+    };
+
+    $scope.pie = function () {
+        document.getElementById('myCoolChart').setAttribute('type', 'Pie');
+        $scope.updateData();
+    };
+
+    $scope.doughnut = function () {
+        document.getElementById('myCoolChart').setAttribute('type', 'Doughnut');
+        $scope.updateData();
+    };
+
+    $scope.generateData();
+
+
+}])
+;
 
 patients.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/patients', {
